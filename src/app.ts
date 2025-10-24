1904c7f48fd0174534bd25e05142bede67f54067
oussamaamou <amoussdark@gmail.com>
2025-10-24 17:16:03 +0100

    chore: intégration de la configuration Passport dans l’application Express

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import type { Application, Request, Response } from 'express';
 import Authroutes from './api/routes/authRoutes.ts';
+import cookieParser from 'cookie-parser';
+import passport from 'passport';
+import session from 'express-session';
+import configurePassport from './config/passport.ts';
 
 export default class App {
 
@@ -11,7 +15,28 @@ export default class App {
     this.app = express();
     this.authroutes = new Authroutes();
     this.app.use(express.json());
+    this.setupMiddleware();
     this.setupRoutes();
+  }
+
+  private setupMiddleware(): void {
+    this.app.use(express.json());
+    this.app.use(cookieParser());
+
+    this.app.use(
+      session({
+        secret: process.env.SECRET_KEY || '',
+        resave: false,
+        saveUninitialized: false,
+        cookie: { secure: false },
+      })
+    );
+
+    this.app.use(passport.initialize());
+    this.app.use(passport.session());
+
+    configurePassport();
+
   }
 
   private setupRoutes(): void {
