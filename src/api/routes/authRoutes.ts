7adc08f5a7649a16a8310d09fbca33b3db3731c7
oussamaamou <amoussdark@gmail.com>
2025-10-28 12:08:20 +0100

    refactor: ajustement des routes d’authentification pour intégrer le flux SSO

--- src/api/routes/authRoutes.ts
+++ src/api/routes/authRoutes.ts
@@ -3,7 +3,7 @@ import Authcontroller from "../controllers/authController.ts";
 import Authmiddleware from "../middlewares/authMiddleware.ts";
 import type { Request, Response } from "express";
 import passport from "passport";
-import { googleAuth, googleAuthCallback, protectRoute } from "../controllers/SSOController.ts";
+import { googleAuth, googleAuthCallback } from "../controllers/SSOController.ts";
 
 class Authroutes{
 
@@ -25,14 +25,31 @@ class Authroutes{
 			passport.authenticate('google', { session: false, failureRedirect: '/api/auth/google/failure'}), 
 			googleAuthCallback
 		);
+		
 		this.router.get('/google/failure', (req: Request, res:Response) => {
 			res.status(401).json({ message: 'Google authentification failed'});
 		});
-		this.router.get('/profile', protectRoute, (req: Request, res: Response) => {
-			
+
+		this.router.get('/profile', authmiddleware.protectRoute, (req: Request, res: Response) => {
 			const user = (req as any).user;
-			res.status(200).json({profile: user })
-		});
+			
+			if (!user) {
+				res.status(404).json({ message: 'User not found' });
+				return;
+			}
+			
+			res.status(200).json({ 
+				profile: {
+					id: user._id,
+					firstname: user.firstname,
+					lastname: user.lastname,
+					email: user.email,
+					role: user.role,
+					verified: user.verified,
+					authProvider: user.authProvider
+				}
+			});
+        });
 
 	}
 }
